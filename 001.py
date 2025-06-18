0abe2a493118a9202e15ef4c3296fab2dd06fadb
Zac <a19860101@gmail.com>
2025-06-18 21:06:07 +0800

    first

---
+++ 001.py
@@ -0,0 +1,20 @@
+# print('hello')
+# print("hello")
+# print(123)
+# print('234')
+
+# 註解 Ctrl + / (不可以用數字鍵盤的/)
+'''
+多行註解
+多行註解
+多行註解
+'''
+"""
+多行註解
+多行註解
+多行註解
+"""
+
+# \n 跳脫字元 - 換行 new line
+# print('john','mary',end='@\n')
+print('weryewry',345234523,sep='-')
