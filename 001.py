8773c6b0bd6b8aa6d787dda68418b9f866602187
Zac <a19860101@gmail.com>
2025-06-18 21:36:23 +0800

    kwlist

--- 001.py
+++ 001.py
@@ -18,3 +18,20 @@
 # \n 跳脫字元 - 換行 new line
 # print('john','mary',end='@\n')
 print('weryewry',345234523,sep='-')
+
+# 變數
+# 大小寫有差
+# 首字不可使用數字或特殊符號
+# 不可使用保留字（關鍵字）
+
+a = 100
+A = 200
+b = 'hello'
+a123 = 123
+
+print(b)
+print('b')
+
+# 查詢關鍵字
+import keyword
+print(keyword.kwlist)
