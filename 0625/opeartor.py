1f234893e9c631b573a8497b9fd86e591d6d7f76
Zac <a19860101@gmail.com>
2025-06-25 19:11:13 +0800

    20250625

---
+++ 0625/opeartor.py
@@ -0,0 +1,31 @@
+x = 10
+y = 3
+x += y
+print(x) # 13
+x -= y
+print(x) # 10
+x *= y
+print(x) # 30
+x /= y
+print(x) # 10
+x %= y
+print(x) # 1
+x **= y
+print(x) # 1
+x //= y
+print(x) # 0
+
+# print(x + y)
+# print(x - y)
+# print(x * y)
+# print(x / y)
+# print(x % y)
+# print(x ** y)
+# print(x // y)
+
+# print(x > y) #t
+# print(x < y) #f
+# print(x >= y) #t
+# print(x <= y) #f
+# print(x == y) #f
+# print(x != y) #t
