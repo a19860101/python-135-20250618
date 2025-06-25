5c6084348d5feb0f80b4f5bd5e86eea922d5df4a
Zac <a19860101@gmail.com>
2025-06-25 19:32:26 +0800

    20250625

--- 0625/opeartor.py
+++ 0625/opeartor.py
@@ -1,20 +1,7 @@
 x = 10
 y = 3
-x += y
-print(x) # 13
-x -= y
-print(x) # 10
-x *= y
-print(x) # 30
-x /= y
-print(x) # 10
-x %= y
-print(x) # 1
-x **= y
-print(x) # 1
-x //= y
-print(x) # 0
 
+# 算術
 # print(x + y)
 # print(x - y)
 # print(x * y)
@@ -23,9 +10,53 @@ print(x) # 0
 # print(x ** y)
 # print(x // y)
 
+# 比較
 # print(x > y) #t
 # print(x < y) #f
 # print(x >= y) #t
 # print(x <= y) #f
 # print(x == y) #f
 # print(x != y) #t
+
+# 指定
+# x += y
+# print(x) # 13
+# x -= y
+# print(x) # 10
+# x *= y
+# print(x) # 30
+# x /= y
+# print(x) # 10
+# x %= y
+# print(x) # 1
+# x **= y
+# print(x) # 1
+# x //= y
+# print(x) # 0
+
+# print(x is y)
+# print(x is not y)
+
+# 可迭代物件
+# print(x in [11,20,30,40])
+# print(x not in [11,20,30,40])
+
+a = 'apple'
+b = 'banana'
+
+d = 0
+e = None
+f = ''
+g = ' '
+h = '0'
+
+
+print(not d)
+print(not e)
+print(not f)
+print(not g)
+print(not h)
+
+# print( x > 0 and y < 0)
+# print( x > 0 or y < 0)
+
