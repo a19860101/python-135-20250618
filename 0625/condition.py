602a549431914c107d59ebaf6a06dc7e8a90693c
Zac <a19860101@gmail.com>
2025-06-25 20:07:44 +0800

    20250625

---
+++ 0625/condition.py
@@ -0,0 +1,26 @@
+x = 0
+
+if x > 0 :
+    print('success')
+
+if x > 0:
+    print('success')
+else:
+    print('error')
+
+if x > 0:
+    print('正')
+elif x < 0:
+    print('負')
+else:
+    print('0')
+
+score = 70
+
+if score >= 90:
+    print('A')
+elif score >= 60:
+    print('B')
+else:
+    print('C')
+
