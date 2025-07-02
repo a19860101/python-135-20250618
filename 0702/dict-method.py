9c9613845fa9135cc68e3fe5712ff2e7a53a7a21
Zac <a19860101@gmail.com>
2025-07-02 19:52:18 +0800

    20250702

--- 0702/dict-method.py
+++ 0702/dict-method.py
@@ -6,13 +6,27 @@ d = {
 # keys
 # print(d.keys())
 # values
-print(d.values())
+# print(d.values())
 # items
-print(d.items())
+# print(d.items())
 
 # for i in d.values():
 #     print(i)
 
-for k,v in d.items():
-    print(f'{k}: {v}')
+# for k,v in d.items():
+#     print(f'{k}: {v}')
 
+
+# key存在就修改，key不存在就新增
+# d['name'] = 'Mary'
+# d['birth'] = '1999/12/23'
+
+# key存在就修改，key不存在就新增
+# d.update({'name': 'Mary'})
+# d.update({'birth': '1999/12/23'})
+
+# key存在就維持預設，key不存在就新增
+d.setdefault('name', 'Mary')
+d.setdefault('birth', '1999/12/23')
+
+print(d)
