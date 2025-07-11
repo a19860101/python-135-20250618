608779500b0608a2859e2dc8bfa90ce31a99fb66
Zac <a19860101@gmail.com>
2025-07-11 20:01:01 +0800

    20250711

--- 0711/pandas-5.py
+++ 0711/pandas-5.py
@@ -2,12 +2,17 @@ import pandas as pd
 
 data = pd.read_csv('Restaurant_C_f.csv')
 
-# print(data.iloc[0])
+print(data.iloc[0])
 
 # print(data[data['Region']=='新北市'])
 # print(data[data['Add'].str.contains('新北')])
 
-sb = data[data['Add'].str.contains('新北')]
-ty = data[data['Region'] == '桃園市']
-print(sb['Name'])
-print(ty['Name'])
+myDatas = data[data['Add'].str.contains('新北')]
+# ty = data[data['Region'] == '桃園市']
+# print(sb['Name'])
+# print(ty['Name'])
+
+myDatas = myDatas[["Name",'Add','Tel','Opentime']]
+
+# myDatas.to_excel('data.xlsx')
+#  需要安裝 openpyxl
