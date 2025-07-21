1a7db07a22f7edbc7d6c3c1de27eff8c3b503747
Zac <a19860101@gmail.com>
2025-07-21 19:15:43 +0800

    20250721

---
+++ 0721/error-handling-1.py
@@ -0,0 +1,23 @@
+x = 'hello'
+# try:
+#     print(x)
+# except NameError:
+#     print('Name Error')
+# except:
+#     print('error')
+# else:
+#     print('else')
+# finally:
+#     print('Complete')
+
+i = -10
+# if i < 0:
+#     raise Exception('請輸入正整數')
+
+if i>0:
+    print('ok')
+else:
+    raise Exception('請輸入正整數')
+
+# Built in Exceptions
+# https://docs.python.org/3/library/exceptions.html
